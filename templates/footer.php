91ae15c6aa8c4aef20da2a432cda8fd0ed288985
Nael Concescu <djanym@users.noreply.github.com>
2025-08-06 19:38:25 +0300

    adds pages content and styles

---
+++ templates/footer.php
@@ -0,0 +1,77 @@
+<footer class="site-footer">
+    <div class="site-container">
+        <div class="footer-columns">
+            <div class="footer-column">
+                <h3 class="section-heading">Our Location</h3>
+                <address>
+                    Pervomaisk 13a, Floreni, MD 2031<br>
+                    <a href="mailto:sales@zzl-precast.com">sales@zzl-precast.com</a>
+                </address>
+
+                <h3 class="section-heading">CALL US</h3>
+                <p><a href="tel:0227552515">022-755-2515</a></p>
+            </div>
+
+            <div class="footer-column">
+                <h3 class="section-heading">SEND US A MESSAGE</h3>
+                <form action="/contact/?success=1" method="post" class="contact-form">
+                    <div class="form-group">
+                        <input type="text" id="name" name="name" placeholder="Your Name" required>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" id="email" name="email" placeholder="Your Email" required>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="tel" id="phone" name="phone" placeholder="Your Phone" required>
+                    </div>
+
+                    <fieldset>
+                        <legend>City</legend>
+                        <div class="radio-group">
+                            <input type="radio" id="nh" name="city" value="Chișinau" checked>
+                            <label for="ch">Chișinau</label>
+                        </div>
+                        <div class="radio-group">
+                            <input type="radio" id="vt" name="City" value="Balți">
+                            <label for="bl">Balți</label>
+                        </div>
+                        <div class="radio-group">
+                            <input type="radio" id="me" name="City" value="Tiraspoli">
+                            <label for="tr">Tiraspoli</label>
+                        </div>
+                        <div class="radio-group">
+                            <input type="radio" id="ma" name="City" value="Other">
+                            <label for="other">Other</label>
+                        </div>
+                    </fieldset>
+
+                    <div class="form-group">
+                        <textarea id="your-message" name="message" placeholder="Your Message" rows="3" required></textarea>
+                    </div>
+
+                    <button type="submit" class="btn">Send</button>
+                </form>
+            </div>
+
+            <div class="footer-column">
+                <h3 class="section-heading">FOLLOW US</h3>
+                <ul class="social-links">
+                    <li><a href="https://www.facebook.com/zzl/">Facebook</a></li>
+                    <li><a href="https://twitter.com/zzlprecast">Twitter</a></li>
+                    <li><a href="https://www.instagram.com/zzlprecast/">Instagram</a></li>
+                    <li><a href="https://www.pinterest.com/zzlprecast/">Pinterest</a></li>
+                    <li><a href="https://www.youtube.com/c/zzlprecast">Youtube</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-credit">
+            All content &copy; 2023 ZZL Precast Concrete. All rights reserved.
+        </div>
+    </div>
+</footer>
+</div> <!-- .wrapper -->
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Precast Concrete Products Supplier - ZZL Company</title>
+    <link rel="stylesheet" href="<?php echo SITE_URL; ?>/assets/css/styles.css" type="text/css">
+    <script src="<?php echo SITE_URL; ?>/assets/js/main.js"></script>
+</head>
+
+<body>
+<header>
+    <nav class="nav">
+        <a href="<?php echo SITE_URL; ?>/home">Home</a>
+        <a href="<?php echo SITE_URL; ?>/services">Services</a>
+        <a href="<?php echo SITE_URL; ?>/contact">Contact Us</a>
+    </nav>
+</header>
+
+<div class="wrapper">
